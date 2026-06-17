2e78d10d0d294fe9952872262b9d490b32c27a98
problemphobia-maker <problemphobia@gmail.com>
2026-06-17 11:30:16 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,20 @@
 export const metadata = {
   title: "AI Solutions",
-  description: "AI Tools Directory",
+  description: "AI Tools Directory"
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html>
+      <body
+        style={{
+          background:"#0f172a",
+          color:"white",
+          margin:0
+        }}
+      >
+        {children}
+      </body>
     </html>
   );
 }
